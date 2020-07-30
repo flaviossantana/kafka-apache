dfb15471bcf64f0659be5ff1e78dfddffc0ee036
Flávio Santana <9090flavio@gmail.com>
2020-07-30 20:42:04 -0300

    [KAFKA][INTRODUÇÃO A STREAMS EM MICROSSERVIÇOS][CRIANDO PRODUTORES EM JAVA]

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # kafka-apache
 Teste de Integração com Apache Kafka
+
+- Iniciar Zookeeper:
+<br><i>.\bin\windows\zookeeper-server-start.bat config/zookeeper.properties</i> 
+
+- Iniciar Kafka:
+<br><i>.\bin\windows\kafka-server-start.bat config/server.properties</i>
---
+++ src/main/java/br/com/kafka/NewOrder.java
@@ -0,0 +1,43 @@
+package br.com.kafka;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class NewOrder {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        KafkaProducer producer = new KafkaProducer<String, String>(properties());
+        String message = "CHAIR,2349";
+        ProducerRecord record = new ProducerRecord<String, String>("STORE_NEW_ORDER", message, message);
+        producer.send(record, new Callback() {
+            public void onCompletion(RecordMetadata data, Exception ex) {
+                if(ex != null){
+                    ex.printStackTrace();
+                    return;
+                }
+                System.out.println(
+                        "SENT SUCCESSFULY: " +
+                        "TOPIC: " +
+                        data.topic() + " | " +
+                        "PARTITION: " +
+                        data.partition() +" | " +
+                        "OFFSET: " +
+                        data.offset() +" | "+
+                        "TIMESTAMP: " +
+                        data.timestamp());
+            }
+        }).get();
+    }
+
+    private static Properties properties() {
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        return properties;
+    }
+
+}
