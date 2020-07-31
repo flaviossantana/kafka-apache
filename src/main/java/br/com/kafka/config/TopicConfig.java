a18fd50df8e596af87e3deebea8b29b652acb228
Flávio Santana <9090flavio@gmail.com>
2020-07-31 11:38:17 -0300

    [KAFKA][INTRODUÇÃO A STREAMS EM MICROSSERVIÇOS][PARALELIZANDO TAREFAS EM UM SERVIÇO][VÁRIOS CONSUMIDORES E PRODUTORES]

--- src/main/java/br/com/kafka/config/TopicConfig.java
+++ src/main/java/br/com/kafka/config/TopicConfig.java
@@ -2,6 +2,8 @@ package br.com.kafka.config;
 
 public class TopicConfig {
 
+    public static final String STORE_ALL_TOPICS = "STORE.*";
     public static final String STORE_NEW_ORDER = "STORE_NEW_ORDER";
+    public static final String STORE_SEND_EMAIL = "STORE_SEND_EMAIL";
 
 }
