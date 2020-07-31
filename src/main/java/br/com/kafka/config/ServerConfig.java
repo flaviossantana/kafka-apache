a033ff5c269b181117170895c8d852a8b6c88c8b
Flávio Santana <9090flavio@gmail.com>
2020-07-30 22:04:03 -0300

    [KAFKA][INTRODUÇÃO A STREAMS EM MICROSSERVIÇOS][CRIANDO CONSUMIDORES EM JAVA]

---
+++ src/main/java/br/com/kafka/config/ServerConfig.java
@@ -0,0 +1,10 @@
+package br.com.kafka.config;
+
+public class ServerConfig {
+
+    public static final String IP_PORT = "127.0.0.1:9092";
+
+    private ServerConfig() {
+        super();
+    }
+}
