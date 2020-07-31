99c92470b39660b0ee4a4dce8344d3faf66f8bdf
Flávio Santana <9090flavio@gmail.com>
2020-07-30 22:04:37 -0300

    [KAFKA][INTRODUÇÃO A STREAMS EM MICROSSERVIÇOS][CRIANDO CONSUMIDORES EM JAVA]

---
+++ src/main/java/br/com/kafka/service/FraudDetectorService.java
@@ -0,0 +1,65 @@
+package br.com.kafka.service;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+import static br.com.kafka.config.ServerConfig.IP_PORT;
+import static br.com.kafka.config.TopicConfig.STORE_NEW_ORDER;
+
+public class FraudDetectorService {
+
+    public static void main(String[] args) {
+
+        KafkaConsumer consumer = new KafkaConsumer<String, String>(properties());
+        consumer.subscribe(Collections.singletonList(STORE_NEW_ORDER));
+
+        while (true){
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+            processorRecord(records);
+        }
+
+    }
+
+    private static Properties properties() {
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, IP_PORT);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudDetectorService.class.getSimpleName());
+        return properties;
+    }
+
+    private static void processorRecord(ConsumerRecords<String, String> records) {
+        if (!records.isEmpty()) {
+            records.forEach(record -> {
+                printOrder(record);
+                sleep();
+            });
+        }
+    }
+
+    private static void printOrder(ConsumerRecord<String, String> record) {
+        System.out.println("----------------------------------------------------");
+        System.out.println("PROCESSING NEW ORDER");
+        System.out.println(record.key());
+        System.out.println(record.value());
+        System.out.println(record.offset());
+        System.out.println(record.partition());
+    }
+
+    private static void sleep() {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
