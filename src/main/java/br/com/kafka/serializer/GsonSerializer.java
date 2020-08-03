1c9fe9dca1289d39b17f0159eb0b2e29d62c4bc4
Flávio Santana <9090flavio@gmail.com>
2020-08-03 15:24:53 -0300

    [KAFKA][INTRODUÇÃO A STREAMS EM MICROSSERVIÇOS][PARALELIZANDO TAREFAS EM UM SERVIÇO][EXTRAINDO NOSSA CAMADA DE PRODUCER]

---
+++ src/main/java/br/com/kafka/serializer/GsonSerializer.java
@@ -0,0 +1,12 @@
+package br.com.kafka.serializer;
+
+import com.google.gson.Gson;
+import org.apache.kafka.common.serialization.Serializer;
+
+public class GsonSerializer<T> implements Serializer<T> {
+
+    @Override
+    public byte[] serialize(String s, T obj) {
+        return new Gson().toJson(obj).getBytes();
+    }
+}
