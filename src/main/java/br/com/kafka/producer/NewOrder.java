3d5e5b3be1810c40681628c5773f1ec04001df8e
Flávio Santana <9090flavio@gmail.com>
2020-08-03 15:24:35 -0300

    [KAFKA][INTRODUÇÃO A STREAMS EM MICROSSERVIÇOS][PARALELIZANDO TAREFAS EM UM SERVIÇO][EXTRAINDO NOSSA CAMADA DE PRODUCER]

--- src/main/java/br/com/kafka/producer/NewOrder.java
+++ src/main/java/br/com/kafka/producer/NewOrder.java
@@ -1,7 +1,9 @@
 package br.com.kafka.producer;
 
+import br.com.kafka.bean.Order;
 import br.com.kafka.core.ProducerService;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
@@ -11,11 +13,19 @@ import static br.com.kafka.config.TopicConfig.STORE_SEND_EMAIL;
 public class NewOrder {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        try (ProducerService produce = new ProducerService();) {
-            for (int i = 0; i <= 100; i++) {
-                String user = UUID.randomUUID().toString();
-                produce.send(STORE_NEW_ORDER, user, "USER: " + user + ", BIKE, R$: 4323.00");
-                produce.send(STORE_SEND_EMAIL, user, "USER: " + user + ". Thanks for your purchase!");
+        try (ProducerService orderProducer = new ProducerService<Order>()) {
+            try (ProducerService emailProducer = new ProducerService<String>()) {
+                for (int i = 0; i <= 100; i++) {
+
+                    String orderId = UUID.randomUUID().toString();
+                    String userId = UUID.randomUUID().toString();
+                    BigDecimal amount = BigDecimal.valueOf(Math.random() * 5000 + 1);
+
+                    Order order = new Order(orderId, userId, amount);
+
+                    orderProducer.send(STORE_NEW_ORDER, userId, order);
+                    emailProducer.send(STORE_SEND_EMAIL, userId, "USER: " + userId + ". Thanks for your purchase!");
+                }
             }
         }
     }
