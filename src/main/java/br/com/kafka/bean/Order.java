db080c90e3b2226c7643f1944fbeb2996f5039e2
Flávio Santana <9090flavio@gmail.com>
2020-08-03 15:23:44 -0300

    [KAFKA][INTRODUÇÃO A STREAMS EM MICROSSERVIÇOS][PARALELIZANDO TAREFAS EM UM SERVIÇO][EXTRAINDO NOSSA CAMADA DE PRODUCER]

---
+++ src/main/java/br/com/kafka/bean/Order.java
@@ -0,0 +1,40 @@
+package br.com.kafka.bean;
+
+import java.math.BigDecimal;
+
+public class Order {
+
+    private String id;
+    private String userId;
+    private BigDecimal amount;
+
+    public Order(String id, String userId, BigDecimal amount) {
+        this.id = id;
+        this.userId = userId;
+        this.amount = amount;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+}
